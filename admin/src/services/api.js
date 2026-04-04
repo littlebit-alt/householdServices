002227ff0af8b5d0ad919ca7368207fa2b8adb33
littlebit-alt <zakizakirez7@email.com>
2026-04-04 16:08:01 +0100

    fix url rendeeeer

--- admin/src/services/api.js
+++ admin/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://householdservices.onrender.com/api',
+  baseURL: 'https://householdservices.onrender.com',
 });
 
 api.interceptors.request.use((config) => {
